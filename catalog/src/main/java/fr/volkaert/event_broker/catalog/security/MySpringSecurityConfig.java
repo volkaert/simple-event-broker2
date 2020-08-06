98b136301b2480504a2d08fce53ac71d8e631f9f
Fabrice VOLKAERT <fabrice_volkaert@yahoo.fr>
2020-08-06 12:03:54 +0200

    Catalog API secured using BasicAuth

---
+++ catalog/src/main/java/fr/volkaert/event_broker/catalog/security/MySpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package fr.volkaert.event_broker.catalog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class MySpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationEntryPoint authEntryPoint;
+
+    @Value("${broker.auth-client-id}")
+    String authClientId;
+
+    @Value("${broker.auth-client-secret}")
+    String authClientSecret;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(authClientId).password(authClientSecret).roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- commons/src/main/java/fr/volkaert/event_broker/catalog_client/CatalogClientConfig.java
+++ commons/src/main/java/fr/volkaert/event_broker/catalog_client/CatalogClientConfig.java
@@ -1,6 +1,7 @@
 package fr.volkaert.event_broker.catalog_client;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +10,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CatalogClientConfig {
 
+    @Value("${broker.auth-client-id-for-catalog}")
+    String authClientIdForCatalog;
+
+    @Value("${broker.auth-client-secret-for-catalog}")
+    String authClientSecretForCatalog;
+
     @Bean
     @Qualifier("RestTemplateForCatalogClient")
     public RestTemplate restTemplateForCatalogClient(RestTemplateBuilder builder) {
-        // Do any additional configuration here
-        return builder.build();
+        return builder
+                .basicAuthentication(authClientIdForCatalog, authClientSecretForCatalog)
+                .build();
     }
 }
