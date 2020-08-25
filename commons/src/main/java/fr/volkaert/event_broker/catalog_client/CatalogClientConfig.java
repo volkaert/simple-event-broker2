bba23f7e3935172d873acd1c592a3cfbe11124fd
Fabrice VOLKAERT <fabrice_volkaert@yahoo.fr>
2020-08-25 15:40:10 +0200

    Service Discovery added for Catalog

--- commons/src/main/java/fr/volkaert/event_broker/catalog_client/CatalogClientConfig.java
+++ commons/src/main/java/fr/volkaert/event_broker/catalog_client/CatalogClientConfig.java
@@ -3,6 +3,7 @@ package fr.volkaert.event_broker.catalog_client;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +19,7 @@ public class CatalogClientConfig {
 
     @Bean
     @Qualifier("RestTemplateForCatalogClient")
+    @LoadBalanced
     public RestTemplate restTemplateForCatalogClient(RestTemplateBuilder builder) {
         return builder
                 .basicAuthentication(authClientIdForCatalog, authClientSecretForCatalog)
